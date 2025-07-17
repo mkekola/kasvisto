4565c40ac48a404abbe60f0ab47c9d8c6fe3e288
Maria Kekola <maria.kekola@helsinki.fi>
2025-07-17 19:56:22 +0300

    Added option to add plants to database.

--- app.py
+++ app.py
@@ -12,6 +12,22 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_plant")
+def new_plant():
+    return render_template("new_plant.html")
+
+@app.route("/create_plant", methods=["POST"])
+def create_plant():
+    plant_name = request.form["plant_name"]
+    light = request.form["light"]
+    care_info = request.form["care_info"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO plants (plant_name, light, care_info, user_id) VALUES (?, ?, ?, ?)"
+    db.execute(sql, [plant_name, light, care_info, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -42,16 +58,20 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-    password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
-    if check_password_hash(password_hash, password):
-        session["username"] = username
-        return redirect("/")
-    else:
-        return "VIRHE: väärä tunnus tai salasana"
+        if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
+            session["username"] = username
+            return redirect("/")
+        else:
+            return "VIRHE: väärä tunnus tai salasana"
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE plants (
+    id INTEGER PRIMARY KEY,
+    plant_name TEXT,
+    light TEXT,
+    care_info TEXT,
+    user_id INTEGER REFERENCES users
+);
